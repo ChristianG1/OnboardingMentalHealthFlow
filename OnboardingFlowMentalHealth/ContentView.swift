84fe41456dcca8c410c222551d6dca0b7da69fe9
Christian George <castellanosgeorgec@gmail.com>
2024-08-19 15:46:28 -0600

    Initial Commit

---
+++ OnboardingFlowMentalHealth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnboardingFlowMentalHealth
+//
+//  Created by Christian George on 19/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnboardingFlowMentalHealth/OnboardingFlowMentalHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowMentalHealthApp.swift
+//  OnboardingFlowMentalHealth
+//
+//  Created by Christian George on 19/08/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowMentalHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
